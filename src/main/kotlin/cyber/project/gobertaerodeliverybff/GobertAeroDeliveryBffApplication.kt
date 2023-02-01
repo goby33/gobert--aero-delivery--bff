dfe4fdcd30e24c6e353c9a5a725c116463d17ea8
Goby33 <martin.gobert@hotmail.fr>
2023-02-01 21:15:01 +0100

    first commit

---
+++ src/main/kotlin/cyber/project/gobertaerodeliverybff/GobertAeroDeliveryBffApplication.kt
@@ -0,0 +1,11 @@
+package cyber.project.gobertaerodeliverybff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GobertAeroDeliveryBffApplication
+
+fun main(args: Array<String>) {
+    runApplication<GobertAeroDeliveryBffApplication>(*args)
+}
---
+++ src/test/kotlin/cyber/project/gobertaerodeliverybff/GobertAeroDeliveryBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package cyber.project.gobertaerodeliverybff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GobertAeroDeliveryBffApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
